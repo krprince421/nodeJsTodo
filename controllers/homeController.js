f5639428a8420fc4643e7d4d82841db31f4179d4
krprince421 <61113784+krprince421@users.noreply.github.com>
2022-10-19 16:47:08 +0530

    more functionality added

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,32 @@
 const User = require('../models/user') ;
 const Todo = require('../models/todo') ;
-
+/*
 module.exports.home = async function( req , res ){
 
     Todo.find({}).populate('user').exec( function( err , todos ){
         return res.render('home' ,{
+            todos : todos
+        })
+    })
+
+}
+*/
+
+module.exports.home = async function( req , res ){
+    if( req.user ){
+        console.log( "User id is " , req.user._id ) ;
+
+        Todo.find({ user : req.user._id }).populate('user').exec( function( err , todos){
+            return res.render('home' , {
+                todos : todos
+            })
+        })
+        return ;
+    }
+    
+    //console.log( "id : ",  req.user._id ) ;
+    Todo.find({}).populate('user').exec( function( err , todos){
+        return res.render('home' , {
             todos : todos
         })
     })
